d8c8cf29d3832f2f91904d278bc5bf9706299bee
Atticus Zambrana <angusbeefsteak14@gmail.com>
2019-04-27 18:33:12 -0700

    k thx

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,14 +1,14 @@
-var baseURL = "https://akshatmittal.com/youtube-realtime/";
+var baseURL = "https://atticusthecoder.github.io/youtube-realtime/";
 if (typeof isEmbed == 'undefined') isEmbed = 0;
 if (typeof isCustomPage == 'undefined') isCustomPage = 0;
-/*
+
 if (window.location.hostname.indexOf("local.akshatmittal.com") < 0) {
     if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
     if (location.hostname.indexOf("akshatmittal.com") == -1) window.location.replace(baseURL + location.hash);
     if ((window.top !== window.self) && isEmbed == 0) window.top.location.replace(window.self.location.href);
     if ((window.top == window.self) && isEmbed) window.top.location.replace(baseURL + location.hash);
 }
-*/
+
 Array.prototype.shuffle = function () {
     var i = this.length,
         j, temp;
